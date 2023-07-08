a99916fb47ded7e71d8d3d26fd7e13924acbe165
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-07-08 13:32:43 +0530

    Update Polymorphism.py

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,24 +1,22 @@
 #polymorphism example
 class Vehicle:
-    def _init_(self,make,model,horsepower):
-        self.make = make
-        self.model=model
-        self.horsepower=horsepower
-        def move(self):
-            print("Quater Mile time!")
+  def __init__(self, make, model):
+    self.make = make
+    self.model = model
+  def move(self):
+    print("Quater mile time!")
 class Car(Vehicle):
-    pass
+  pass
 class Boat(Vehicle):
-    def move(self):
-        print("Sail!")
+  def move(self):
+    print("Sail off!")
 class Plane(Vehicle):
-    def move(self):
-        print("Fly!")
-car1 = Car("Nissan","GTR",565)
-boat1 = Boat("Yamaha","Speedboat",352)
-plane1 = Plane("Boeing", "747", 59934)
-for x in (car1,boat1,plane1):
-    print (x.make)
-    print (x.model)
-    x.move()
-    
+  def move(self):
+    print("Take off!")
+car1 = Car("Nissan", "GTR") #Create a Car object
+boat1 = Boat("Yamaha", "Speeding boat") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+for x in (car1, boat1, plane1):
+  print(x.make, x.model)
+  x.move()
+
