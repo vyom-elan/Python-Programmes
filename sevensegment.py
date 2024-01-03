6ee818211767dc8f13b0cfe8433242058833d9f5
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-01-03 15:19:37 +0530

    Update sevensegment.py

--- sevensegment.py
+++ sevensegment.py
@@ -1,4 +1,6 @@
-seven_segment_so = {
+#Python Programme displaying 4 Seven Segment
+#Sample Number to be displayed = 64:53
+seven_segment= {
     '0': 'abcdef',
     '1': 'bc',
     '2': 'abdeg',
@@ -10,49 +12,40 @@ seven_segment_so = {
     '8': 'abcdefg',
     '9': 'abcdfg'
 }
-seven_segment_st = {
-    '0': 'abcdef',
-    '1': 'bc',
-    '2': 'abdeg',
-    '3': 'abcdg',
-    '4': 'bcfg',
-    '5': 'acdfg',
-    '6': 'acdefg',
-    '7': 'abc',
-    '8': 'abcdefg',
-    '9': 'abcdfg'
-}
-seven_segment_mo = {
-    '0': 'abcdef',
-    '1': 'bc',
-    '2': 'abdeg',
-    '3': 'abcdg',
-    '4': 'bcfg',
-    '5': 'acdfg',
-    '6': 'acdefg',
-    '7': 'abc',
-    '8': 'abcdefg',
-    '9': 'abcdfg'
-}
-seven_segment_mt = {
-    '0': 'abcdef',
-    '1': 'bc',
-    '2': 'abdeg',
-    '3': 'abcdg',
-    '4': 'bcfg',
-    '5': 'acdfg',
-    '6': 'acdefg',
-    '7': 'abc',
-    '8': 'abcdefg',
-    '9': 'abcdfg'
-}
-def display_number(number):
-    pattern = seven_segment_so[str(number)]
 
+def display_number_so(number):
+    pattern = seven_segment[str(number)]
     for segment in 'abcdefg':
         if segment in pattern:
             print('1', end=' ')  #give res in the form of 0 and 1
         else: 
             print('0', end=' ')
     print()
-display_number(2) 
+def display_number_st(number):
+    pattern = seven_segment[str(number)]
+    for segment in 'abcdefg':
+        if segment in pattern:
+            print('1', end=' ')  #give res in the form of 0 and 1
+        else: 
+            print('0', end=' ')
+    print()
+def display_number_mo(number):
+    pattern = seven_segment[str(number)]
+    for segment in 'abcdefg':
+        if segment in pattern:
+            print('1', end=' ')  #give res in the form of 0 and 1
+        else: 
+            print('0', end=' ')
+    print()
+def display_number_mt(number):
+    pattern = seven_segment[str(number)]
+    for segment in 'abcdefg':
+        if segment in pattern:
+            print('1', end=' ')  #give res in the form of 0 and 1
+        else: 
+            print('0', end=' ')
+    print()
+display_number_so(3)
+display_number_st(5)
+display_number_mo(4)
+display_number_mt(6)
